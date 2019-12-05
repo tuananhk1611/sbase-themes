1326981b64ed44ca987c50afef37dda21cd81e8f
NGUYENTUANANH <tuananh@brodev.com>
2019-12-05 14:50:59 +0700

    Style theme

--- src/templates/Page/AboutUs.vue
+++ src/templates/Page/AboutUs.vue
@@ -1,100 +1,57 @@
 <template>
   <div id="about-us-page" class="about-us-page">
-    <div v-if="page.title" class="container pt40">
-      <div class="text-align-center text-align-left-md cart__breadcrumbs">
-        <Breadcrumbs :routes="routes" :current="page.title"></Breadcrumbs>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-4 figure-about">
+          <img src="../../assets/images/about-midsleek.png" alt="about-midsleek">
+        </div>
       </div>
-      <section class="mt16 mb5">
-        <div class="row">
-          <div class="col-12">
-            <div class="text-wrapper">
-              <h3
-                class="text-align-center text-align-left-md cart__breadcrumbs"
-              >
-                {{ page.title }}
-              </h3>
+      <div class="row">
+        <div class="col-12">
+          <div class="about-us-content">
+            <h1 class="h1 heading-section-midsleek">
+              Our Story
+            </h1>
+            <div class="row">
+              <div class="col-12 col-sm-8 content-text-about">
+                <p class="mb32">Like every other woman, I got married and got used to the busy life of being a wife,
+                  a mom of two kids and a white-collar worker. Being fed up with all the housework and my career,
+                  I was so stressed out during this time. Luckily, I gradually stepped out of the box
+                  just by spending more time for myself and I realized that only comforting and pampering
+                  myself can help balance my own life.
+                </p>
+                <p class="mb32">It comes to my attention that it is not only me having the trouble but also many of
+                  my friends have these kinds of mid-life crisis. We all somehow were caught up with the
+                  piled up work and forget how importance treasuring ourselves is. Consequently, we neglected
+                  ourselves, depressed and lost the quality of our lives.
+                </p>
+                <p class="mb32">For that reason, I desire to set the inspiration to all my sleek mature girls out
+                  there that we all have our own values and don’t forget to love yourself to brighten your life.
+                  I decided to establish MidSleek - a brand for sleek women who were and being in the midlife period
+                  but still passionate about fashion and beauty, and lust after shoes and bags.
+                </p>
+                <p class="mb32">
+                  MidSleek was found with a mission to inspire women and to embrace their confidence in
+                  fashionable presence. Put ourselves in the minds of our customers, we understand and
+                  respond to customer needs. Our own products, always exploring, devising ways to bring
+                  the best products in both quality and design. The most convenient way, through online
+                  delivery systems nationwide delivery.
+                </p>
+                <p class="sign-text">
+                  Enjoy Your Shopping With Us!
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </section>
-      <div class="row mb55">
-        <div class="col-12 page-content">
-          <div class="mb24" v-html="bodyHtml"></div>
-          <transition name="fade-in-up">
-            <div
-              v-if="submitted || hasError"
-              :class="[
-                'py12 px10 mb16 brdr-1 brdr-cl-white',
-                { 'page-content__alert-success': submitted },
-                { 'page-content__alert-error': hasError },
-              ]"
-            >
-              {{
-                contactPosted
-                  ? $t(
-                      "Thanks for contacting us. We'll get back to you as soon as possible."
-                    )
-                  : $t('Error while sending contact us.')
-              }}
-            </div>
-          </transition>
-          <form
-            id="contact_form"
-            accept-charset="UTF-8"
-            class="row contact-form"
-            @submit.prevent="submitContact"
-          >
-            <div class="col-12 col-sm-6 mb12 contact-form__name">
-              <BaseInput
-                v-model="contact.name"
-                type="text"
-                name="name"
-                autocomplete="name"
-                :placeholder="$t('Name')"
-              />
-            </div>
-            <div class="col-12 col-sm-6 mb12 contact-form__email">
-              <BaseInput
-                v-model="contact.email"
-                type="email"
-                name="email"
-                autocomplete="email"
-                required
-                :placeholder="$t('Email')"
-              />
-            </div>
-            <div class="col-12 col-sm-12 mb12">
-              <BaseInput
-                v-model="contact.phone"
-                type="tel"
-                name="phone"
-                autocomplete="phone"
-                pattern="[0-9\-]*"
-                :placeholder="$t('Phone')"
-              />
-            </div>
-            <div class="col-12 col-sm-12 mb12">
-              <BaseTextarea
-                v-model="contact.message"
-                name="message"
-                spellcheck="false"
-                :placeholder="$t('Message')"
-                :rows="10"
-              />
-            </div>
-
-            <div class="col-12 col-sm-12 mt5 text-align-right">
-              <button
-                type="submit"
-                :class="['btn btn-primary px55', { 'is-loading': isSending }]"
-              >
-                {{ $t('Send') }}
-              </button>
-            </div>
-          </form>
-        </div>
       </div>
     </div>
+    <Newsletter
+      v-if="newsletterSettings"
+      :key="newsletterSettings.id"
+      :settings="newsletterSettings.settings"
+      :data-id="newsletterSettings.id"
+    />
   </div>
 </template>
 
@@ -103,6 +60,7 @@ import { getCoreMixin } from '../../shared/mixin'
 import Breadcrumbs from '../../components/Breadcrumbs'
 import BaseInput from '../../components/BaseInput'
 import BaseTextarea from '../../components/BaseTextarea'
+import Newsletter from '../../sections/Home/Newsletter'
 
 export default {
   name: 'TemplatePageAboutUs',
@@ -110,6 +68,7 @@ export default {
     Breadcrumbs,
     BaseInput,
     BaseTextarea,
+    Newsletter
   },
   mixins: [getCoreMixin('contactUsMixin')],
   props: {
@@ -124,6 +83,13 @@ export default {
     routes: {
       type: Array,
       default: () => [],
+    },
+  },
+  computed: {
+    newsletterSettings() {
+      const setting = this.$getPageSettings('homepage').filter((setting) => setting.type === 'newsletter')
+      if (setting && setting.length > 0) return setting[0]
+      return null
     },
   },
 }
